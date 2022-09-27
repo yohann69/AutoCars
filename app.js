43dab95be5dc136d8531a1d08ffb9ced8e571c92
Yohann <73314529+yohann69@users.noreply.github.com>
2022-09-27 08:09:04 +0200

    Improvments

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const express = require('express');
+const { toUSVString } = require('util');
+const { request } = require('http');
 
 const app = express();
 
@@ -14,9 +16,7 @@ app.use(express.json());
 //     res.send('POST request to the homepage');
 // });
 
-const operations = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/operations.json`)
-)
+const operations = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/operations.json`))
 
 app.get('/api/v1/operations', (req, res) => {
     res.status(200).json({
@@ -28,11 +28,69 @@ app.get('/api/v1/operations', (req, res) => {
     })
 })
 
-app.post('/api/v1/operations', (req, res) => {
 
+/*  Get operations with a specific id */
+app.get('/api/v1/operations/:id', (req, res) => {  // For optional parameters add ? at the end (ex: /api/v1/operations/:id?)
+    // console.log(req.params);
+    const id = parseInt(req.params.id); 
+    const operation = operations.find(element => element.id === id);
+
+    if (!operation) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    } else {
+        res.status(200).json({
+            status: 'success',
+            results: operations.length,
+            data: {
+                operation
+            }
+        })
+    }
+})
+
+app.post('/api/v1/operations', (req, res) => {
+    // console.log(req.body);
+
+    const newID = operations[operations.length - 1].id + 1;
+    const newOperation = Object.assign({ id: newID }, req.body);
+
+    operations.push(newOperation);
+    fs.writeFile(`${__dirname}/dev-data/data/operations.json`, JSON.stringify(operations), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                operation: newOperation
+            }
+        })
+    })
+
+    // res.send('Done');
 })
 
 
+
+app.patch('/api/v1/operations/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const operation = operations.find(element => element.id === id);
+
+    if (!operation) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    } else {
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                operation: '<Updated operation here>'
+            }
+        })
+    }
+})
 
 const port = 3000;
 app.listen(port, () => {
