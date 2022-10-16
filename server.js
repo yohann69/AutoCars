2ef0445c0a6ddb2793ef5b18d4b83013b6a4fae3
Yohann <73314529+yohann69@users.noreply.github.com>
2022-10-16 18:21:59 +0200

    Update Stating mongodb

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-dotenv.config({path: './config.env'})
+dotenv.config({ path: './config.env' });
 
-const app = require('./app')
+const db = process.env.DATABASE_mongodb.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-// console.log(process.env);
+console.log(db)
 
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then(() => console.log("Connected to database"));
+
+const app = require('./app');
+
+const operationScema = new mongoose.Schema({
+    name: String,
+    duration: Number,
+    price: Number
+})
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
