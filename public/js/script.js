9ab63f38aa49ef69abdbf8414f830f26c03cef0f
Yohann <73314529+yohann69@users.noreply.github.com>
2022-10-25 10:49:24 +0200

    Improving stuff

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+// const back = document.querySelectorAll('.back');
+// const modal = document.querySelectorAll('.popupmenu');
+
+
+// for(let i = 0; i < back.length; i++) {
+//     back[i].addEventListener('click', modal[i].classList.add('hidden'));
+// }
