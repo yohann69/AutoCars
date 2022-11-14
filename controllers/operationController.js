6c41d637d9343e788967b3567dff20cd0a0abc98
Yohann <73314529+yohann69@users.noreply.github.com>
2022-11-14 22:46:10 +0100

    APIFeatures Class

--- controllers/operationController.js
+++ controllers/operationController.js
@@ -1,5 +1,6 @@
 const { query } = require('express');
 const Operation = require('./../models/operationModel')
+const APIFeatures = require('./../utils/apiFeatures');
 
 
 exports.aliasCheapestOperation = (req, res, next) => {
@@ -10,55 +11,16 @@ exports.aliasCheapestOperation = (req, res, next) => {
 }
 
 
+
 exports.getAllOperations = async (req, res) => {
     try {
-        const queryObj = { ...req.query }; // ... = destruturing query object
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(field => delete queryObj[field]);
-
-        // Prepare query
-        let queryStr = JSON.stringify(queryObj);
-        // Replace gte gt lte lt with the same word but with a $ in front
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-
-        let query = Operation.find(JSON.parse(queryStr));
-
-        // Sort results
-        if (req.query.sort) {
-            // Sort results by multiple fields (add ',' in request)
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        }
-
-
-        // Limit fields
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-__v');  // - to remove the variable
-        }
-
-
-
-        // Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numOperations = await Operation.countDocuments();
-            if (skip >= numOperations) throw new Error('This page does not exist');
-        }
-
-
-
-
         // Execute query
-        const operations = await query;
+        const features = new APIFeatures(Operation.find(), req.query)
+            .filter()
+            .limitFields()
+            .sort()
+            .paginate();
+        const operations = await features.query;
 
         // Send response
         res.status(200).json({
@@ -68,6 +30,7 @@ exports.getAllOperations = async (req, res) => {
                 operations
             }
         })
+
     } catch (err) {
         res.status(404).json({
             status: 'failed',
