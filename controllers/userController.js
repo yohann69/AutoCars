0e2ab541b04adc083093329c3ab89195648133ba
Yohann <73314529+yohann69@users.noreply.github.com>
2022-12-19 18:08:10 +0100

    Updating user password
    - sending email doesn't work while resetting password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 
 
 exports.getAllUsers = catchAsync(async (req, res) => {
-	const users = await User.find()	
-	
+	const users = await User.find()
+
 	res.status(200).json({
 		status: 'success',
 		results: users.length,
@@ -14,6 +15,29 @@ exports.getAllUsers = catchAsync(async (req, res) => {
 		}
 	})
 })
+
+
+exports.updateMe = (req, res, next) => {
+	// 1) Create error if user POSTs password data
+	if (req.body.password || req.body.passwordConfirm) {
+		return next(
+			new AppError(
+				'This route is not for password updates. Please use /updateMyPassword.',
+				400
+			)
+		)
+	}
+
+	// 2) Update user document
+	
+
+
+	res.status(200).json({
+		status: 'success',
+	})
+
+}
+
 
 
 exports.updateUser = (req, res) => {
--- models/userModel.js
+++ models/userModel.js
@@ -84,6 +84,13 @@ userSchema.pre('save', async function (next) {
 });
 
 
+userSchema.pre('save', function (next) {
+    if(!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+})
+
 userSchema.methods.correctPassword = function (candidatePassword, userPassword) {
     return bcrypt.compare(candidatePassword, userPassword);
 }
@@ -101,15 +108,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
 
 
+
 userSchema.methods.createPasswordResetToken = function () {
     const resetToken = crypto.randomBytes(32).toString('hex');
 
-    this.passwordResetToken = crypto.createHash('sha257').update(resetToken).digest('hex');
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
 
-    this.passwordResetExpires = date.now() + 10 * 60 * 1000;
+    console.log({ resetToken }, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
     return resetToken;
-}
+};
 
 
 const User = mongoose.model('User', userSchema);
